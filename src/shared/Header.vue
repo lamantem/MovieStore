72d3963d5820df0acf7e16aa4a591aac931e4015
lamantem <lamantemachado@gmail.com>
2021-09-18 18:08:45 -0300

    Improvements to Header layout with more clear spacing and adding search field

--- src/shared/Header.vue
+++ src/shared/Header.vue
@@ -1,8 +1,23 @@
 <template>
 <div class="header">
-    <a class="logo" href="#"><font-awesome-icon :icon="['fas', 'film']"/> </a>
-    <a class="navicon" href="#"><font-awesome-icon :icon="['fas', 'heart']"/> </a>
-    <a class="navicon" href="#"><font-awesome-icon :icon="['fas', 'shopping-cart']"/> </a>
+
+  <div id="column1">
+     <a class="logo" href="#">
+      <font-awesome-icon class="oops" :icon="['fas', 'film']" transform="grow-6"  fixed-width /> 
+    </a>
+  </div>
+
+  <div id="column2">
+    <input class="search-field" type="text" placeholder="Search title.."/>
+  </div>
+
+  <div id="column3">
+     <font-awesome-icon class="navicon oops" 
+    :icon="['fas', 'heart']" transform="grow-6"  fixed-width />
+      <font-awesome-icon class="navicon oops" 
+      :icon="['fas', 'shopping-cart']" transform="grow-6"  fixed-width />
+  </div>
+ 
 </div>
 </template>
 
@@ -14,22 +29,40 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+
 .header {
   padding: 14px;
-  background-color: aquamarine;
+  background-color: #7fe4d5;
   overflow:hidden;
+  display: flex;
 }
 
-.header-half {
+.header > div {
+  flex-grow: 1;
+}
+
+#column1, #column3 {
+  width: 25%;
+  display: inline-block;
+}
+
+#column2 {
   width: 50%;
+  display: inline-block;
 }
 
+.search-field {
+  width: 100%;
+}
 .logo {
   float: left;
 }
 
-
 .navicon {
   float: right;
+}
+
+.oops {
+  margin: 0 8px 0 8px;
 }
 </style>
