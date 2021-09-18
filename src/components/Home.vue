29326ac7322bb57739c36d2c0036b2e702fad084
lamantem <lamantemachado@gmail.com>
2021-09-18 13:14:33 -0300

    Creating Home.vue, shared folder and moving Header.vue to shared folder

---
+++ src/components/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <Header></Header>
+  <h1>Welcome</h1>
+  </div>
+</template>
+
+<script>
+import Header from '../shared/Header.vue'
+export default {
+  name: 'Home',
+  components: {
+    Header
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ src/shared/Header.vue
@@ -0,0 +1,39 @@
+<template>
+<div class="header">
+  <div class="header-half left">
+    <a href="#">Logo</a>
+  </div>
+  <div class="header-half right">
+    <a href="#">Favs</a>
+    <a href="#">aaa </a>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.header {
+  padding: 14px;
+  background-color: aquamarine;
+  overflow:hiden;
+}
+
+.header-half {
+  width: 50%;
+}
+
+.left {
+  float: left;
+}
+
+
+.right {
+  float: right;
+}
+</style>
