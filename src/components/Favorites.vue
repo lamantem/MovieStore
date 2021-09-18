2b277b3e9a16672a3b4035222d8a365b9c22f8a7
lamantem <lamantemachado@gmail.com>
2021-09-18 20:53:36 -0300

    Creating Favorites and ShoppingCart components, also setting up their minimal layout structure

---
+++ src/components/Favorites.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <b-button v-b-toggle.sidebar-2>
+      <font-awesome-icon
+        class="navicon icon-spacing"
+        :icon="['fas', 'shopping-cart']"
+        transform="grow-6"
+        fixed-width/>
+    </b-button>
+    <b-sidebar id="sidebar-2" title="Carrinho" shadow>
+      <div class="px-3 py-2">
+        <p>Carrinho</p>
+      </div>
+    </b-sidebar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Favorites",
+  components: {},
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
---
+++ src/components/ShoppingCart.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <b-button v-b-toggle.sidebar-1>
+      <font-awesome-icon
+        class="navicon icon-spacing"
+        :icon="['fas', 'heart']"
+        transform="grow-6"
+        fixed-width/>
+    </b-button>
+    <b-sidebar id="sidebar-1" title="Favoritos" shadow>
+      <div class="px-3 py-2">
+        <p>Favoritos</p>
+      </div>
+    </b-sidebar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ShoppingCart",
+  components: {},
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
